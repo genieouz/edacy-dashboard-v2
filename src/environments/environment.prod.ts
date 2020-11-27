571b12351110ff502708ad00860ad259fcf8ae0d
Ousmane Sakho <genieouzog@gmail.com>
2020-11-27 15:14:15 +0000

    use preprod api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  GRAPHQL_URI: 'https://edacy-api-v2-prod.herokuapp.com/graphql'
+  // GRAPHQL_URI: 'https://edacy-api-v2-prod.herokuapp.com/graphql'
+  GRAPHQL_URI: "https://edacy-api-v2-preprod.herokuapp.com/graphql"
 };
